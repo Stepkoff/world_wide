8341f6916da6e907534d14f1b452c108c18b3885
Stepan Rudiak <Stepan.rudiak95@gmail.com>
2023-12-28 11:20:41 +0100

    theme fix

--- src/features/Theme/ThemeProvider.tsx
+++ src/features/Theme/ThemeProvider.tsx
@@ -1,42 +1,54 @@
-import React, {createContext, ReactNode, useContext, useEffect, useState} from "react";
+import React, {createContext, ReactNode, useContext, useEffect, useReducer, useState} from "react";
 
 export enum Theme {
   SYSTEM = 'system',
   LIGHT = 'light',
-  DARK = 'DARK'
+  DARK = 'dark'
 }
 
-interface ThemeContextType {
-  theme: Theme
+interface ThemeType {
+  theme: Theme,
   setTheme: React.Dispatch<React.SetStateAction<Theme>>
+  isSystemDark: () => boolean
 }
-const ThemeContext = createContext<ThemeContextType>({
+const ThemeContext = createContext<ThemeType>({
   theme: Theme.SYSTEM,
-  setTheme: () => {}
+  setTheme: () => {},
+  isSystemDark: () => false
 })
 
 export const ThemeProvider = ({children}: {children: ReactNode}) => {
+  const [, triggerRerender] = useReducer(prev => prev+1, 0)
   const [theme, setTheme] = useState<Theme>(localStorage.getItem('theme') as Theme ?? Theme.SYSTEM)
   const element = document.documentElement;
   const darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+  // const [themeIcon, setThemeIcon] =
+  //   useState<'light' | 'dark'>(theme === 'system' ? (darkQuery.matches ? 'dark' : 'light') : (theme === 'light' ? 'light' : 'dark'))
 
-  console.log(theme)
   const onWindowMatch = () => {
     if(localStorage.theme === 'dark' || (!(theme in localStorage) && darkQuery.matches)) {
       element.classList.add('dark')
+      // setThemeIcon('dark')
     } else {
       element.classList.remove('dark')
+      // setThemeIcon('light')
     }
+    triggerRerender()
   }
+
+  const isSystemDark = () => element.classList.contains('dark')
 
   const handleChangeDarkQuery = (e:MediaQueryListEvent) => {
     if(!('theme' in localStorage)) {
       if(e.matches) {
         element.classList.add('dark')
+        // setThemeIcon('dark')
       } else {
         element.classList.remove('dark')
+        // setThemeIcon('light')
       }
     }
+    triggerRerender()
   }
 
   useEffect(() => {
@@ -44,10 +56,12 @@ export const ThemeProvider = ({children}: {children: ReactNode}) => {
       case Theme.DARK:
         element.classList.add('dark');
         localStorage.setItem('theme', 'dark')
+        // setThemeIcon('dark')
         break;
-      case Theme.LIGHT:
+      case 'light':
         element.classList.remove('dark');
         localStorage.setItem('theme', 'light');
+        // setThemeIcon('light')
         break;
       default:
         localStorage.removeItem('theme');
@@ -62,7 +76,7 @@ export const ThemeProvider = ({children}: {children: ReactNode}) => {
   }, [theme]);
 
   return (
-    <ThemeContext.Provider value={{theme: theme, setTheme: setTheme}}>
+    <ThemeContext.Provider value={{theme: theme, setTheme: setTheme, isSystemDark}}>
       {children}
     </ThemeContext.Provider>
   )
--- src/widgets/ThemeToggle/ThemeToggle.tsx
+++ src/widgets/ThemeToggle/ThemeToggle.tsx
@@ -2,9 +2,11 @@ import {useRef, useState} from "react";
 import {MenuModal} from "@/shared/ui/MenuModal";
 import s from './themeToggle.module.sass'
 import {Theme, useTheme} from "@/features/Theme";
+import { LuSunMedium } from "react-icons/lu";
+import { BsFillMoonStarsFill } from "react-icons/bs";
 
 export const ToggleTheme = () => {
-  const {theme, setTheme} = useTheme();
+  const {theme, setTheme, isSystemDark} = useTheme();
   const [modalOpened, setModalOpened] = useState(false)
 
   const triggerRef = useRef<HTMLButtonElement>(null);
@@ -20,10 +22,12 @@ export const ToggleTheme = () => {
   }
 
 
+  const icon = theme === 'system' ? (isSystemDark() ? <BsFillMoonStarsFill/> : <LuSunMedium/>) : (theme === 'light' ? <LuSunMedium/> : <BsFillMoonStarsFill/>)
+
   return (
     <div className={''}>
       <button ref={triggerRef} onClick={() => setModalOpened(true)}>
-        theme
+        {icon}
       </button>
       <MenuModal elemRef={triggerRef} isOpened={modalOpened} onClose={() => setModalOpened(false)}>
         <div className={s.menuContainer}>
